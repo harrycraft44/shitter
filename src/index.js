17b395ff39edc7ea4e07a4c9130d7100b5d02fcd
Harry Rogers <78320049+harrycraft44@users.noreply.github.com>
2022-08-21 19:29:05 +0100

    expresss

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 //express 
+//define fs
+const fs = require('fs'); 
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 81;
 const bodyParser = require('body-parser');
+// html server without monogoose
+const http = require('http');
+const ports = 80;
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+
+    switch (req.url){
+        case "/":
+            res.end(fs.readFileSync(`${__dirname}/html/index.html`, "utf8"));
+            break;
+        
+    }
+
+});
+
+server.listen(ports);
